3026771388847938d3ce3efd4e4138bc840ab01c
Kenneth Moore <Kenneth.moore@ungerboeck.com>
2016-02-01 15:11:10 -0600

    Prep for Bower Package

--- Gulpfile.js
+++ Gulpfile.js
@@ -5,6 +5,7 @@
 //get all your task dependencies first
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var rename = require('gulp-rename');
 var sourcemaps = require('gulp-sourcemaps');
 var autoprefixer = require('gulp-autoprefixer');
 var taskListing = require('gulp-task-listing');
@@ -12,15 +13,20 @@ var livereload = require('gulp-livereload');
 var bower = require('main-bower-files');
 var path = require("path");
 
+console.log(__dirname);
 var load_paths = bower({
   base: path.join(__dirname, "bower_components"),
   filter: '**/_*.scss'
 });
+
+var bower_src_paths = [];
+
 for (var i = 0; i < load_paths.length; i++) {
   // we need to use the parent directories of the main files, not the files themselves
   // so ./bower_components/bootstrap-sass-official/assets/stylesheets/_bootstrap.scss
   // becomes ./bower_components/bootstrap-sass-official/assets/stylesheets
   load_paths[i] = path.dirname(load_paths[i]);
+  bower_src_paths[i] = load_paths[i] + '**/*';
 }
 
 //set configuration
@@ -63,6 +69,15 @@ gulp.task('markup-watch', [], function () {
   return gulp.src(config.html)
           .pipe(livereload());
 });
+
+// update bower dependencies located in the vendor folder
+gulp.task('mixer', function () {
+  return gulp.src(bower_src_paths)
+  .pipe(rename(function (path) {
+    path.dirname += "/ciao";
+  }))
+  .pipe(gulp.dest('core/vendor'));
+})
 
 // The default task (called when you run `gulp` from cmd)
 gulp.task('default', taskListing);
